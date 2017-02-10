41f273915b2e7e9554088c4c895778ca23c5b8f5
nielev <447146920@qq.com>
2017-02-10 14:56:55 +0800

    Initial commit

---
+++ app/src/main/kotlin/com/oray/kotlindemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.oray.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv.text = "hello kotlin"
+        tv.textSize = 20.0f
+    }
+}
